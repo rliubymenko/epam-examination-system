62a0769315c5b0890186e1922185738b78a1134b
rliubymenko <rliubymenko@gmail.com>
2022-12-20 22:49:13 +0200

    added handling logic to annotation configuration classes

--- custom_dependency_injection/src/main/java/com/epam/di/configurator/InjectAnnotationConfigurator.java
+++ custom_dependency_injection/src/main/java/com/epam/di/configurator/InjectAnnotationConfigurator.java
@@ -1,11 +1,24 @@
 package com.epam.di.configurator;
 
 import com.epam.di.DependencyInjectionContext;
+import com.epam.di.annotation.PleaseInject;
+
+import java.lang.reflect.Field;
 
 public class InjectAnnotationConfigurator implements AnnotationConfigurator {
 
     @Override
     public void configure(Object candidateForInject, DependencyInjectionContext context) {
-
+        Field[] fields = candidateForInject.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(PleaseInject.class)) {
+                field.setAccessible(true);
+                try {
+                    field.set(candidateForInject, context.getInstance(field.getType()));
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Exception while injecting field " + field.getName() + " into " + candidateForInject.getClass().getSimpleName());
+                }
+            }
+        }
     }
 }
--- custom_dependency_injection/src/main/java/com/epam/di/configurator/ValueAnnotationConfigurator.java
+++ custom_dependency_injection/src/main/java/com/epam/di/configurator/ValueAnnotationConfigurator.java
@@ -1,11 +1,29 @@
 package com.epam.di.configurator;
 
 import com.epam.di.DependencyInjectionContext;
+import com.epam.di.annotation.PleaseValue;
+
+import java.lang.reflect.Field;
 
 public class ValueAnnotationConfigurator implements AnnotationConfigurator {
 
     @Override
     public void configure(Object candidateForInject, DependencyInjectionContext context) {
-
+        Field[] declaredFields = candidateForInject.getClass().getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            if (declaredField.isAnnotationPresent(PleaseValue.class)) {
+                PleaseValue properties = declaredField.getAnnotation(PleaseValue.class);
+                String propertyName = properties.value();
+                String property = context.getPropertiesMap().get(propertyName);
+                if (!property.isBlank()) {
+                    declaredField.setAccessible(true);
+                    try {
+                        declaredField.set(candidateForInject, property);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException("Exception while injecting property " + propertyName + " into " + candidateForInject.getClass().getSimpleName());
+                    }
+                }
+            }
+        }
     }
 }
