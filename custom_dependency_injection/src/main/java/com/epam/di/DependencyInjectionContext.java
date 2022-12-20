c3e1d892b2df8cd0d2e5a70d6d088aeb1adb99d4
rliubymenko <rliubymenko@gmail.com>
2022-12-20 22:47:23 +0200

    configured BeanFactory and DependencyApplicationContext

---
+++ custom_dependency_injection/src/main/java/com/epam/di/DependencyInjectionContext.java
@@ -0,0 +1,100 @@
+package com.epam.di;
+
+import com.epam.di.annotation.PleaseComponentScan;
+import com.epam.di.annotation.PleaseService;
+import com.epam.di.factory.BeanFactory;
+import com.epam.di.util.ResourceUtil;
+import org.reflections.Reflections;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class DependencyInjectionContext {
+
+    private final Map<Class<?>, Map<Class<?>, Object>> interfaceAndImplementationWithInstanceMap = new HashMap<>();
+    private final Set<Class<?>> typesAnnotatedWithPleaseService;
+    private final Map<String, String> propertiesMap;
+    private BeanFactory beanFactory;
+
+    public DependencyInjectionContext(Class<?> configClass) {
+        if (!configClass.isAnnotationPresent(PleaseComponentScan.class)) {
+            throw new RuntimeException("Please specify the package for scan by adding @PleaseComponentScan");
+        } else {
+            propertiesMap = ResourceUtil.getProperties(configClass.getClassLoader());
+            PleaseComponentScan componentScanAnnotation = configClass.getAnnotation(PleaseComponentScan.class);
+            Reflections reflections = new Reflections(componentScanAnnotation.value());
+            typesAnnotatedWithPleaseService = reflections.getTypesAnnotatedWith(PleaseService.class);
+            for (Class<?> implementationClass : typesAnnotatedWithPleaseService) {
+                Class<?>[] interfaces = implementationClass.getInterfaces();
+                Map<Class<?>, Object> implementationWithInstance = new HashMap<>();
+                implementationWithInstance.put(implementationClass, null);
+                if (interfaces.length == 0) {
+                    interfaceAndImplementationWithInstanceMap.put(implementationClass, implementationWithInstance);
+                } else {
+                    for (Class<?> iface : interfaces) {
+                        interfaceAndImplementationWithInstanceMap.put(iface, implementationWithInstance);
+                    }
+                }
+            }
+        }
+    }
+
+    public void applicationInitialization() {
+        for (Class<?> serviceClass : typesAnnotatedWithPleaseService) {
+            getInstance(serviceClass);
+        }
+    }
+
+    public <I> I getInstance(Class<I> type) {
+        if (type.isAnnotationPresent(PleaseService.class)) {
+            Class<? extends I> implementationClass;
+            Map<Class<?>, Object> implementationWithInstanceMap = interfaceAndImplementationWithInstanceMap.get(type);
+            if (implementationWithInstanceMap == null) {
+                implementationWithInstanceMap = interfaceAndImplementationWithInstanceMap.values()
+                        .stream()
+                        .filter(implWithInst -> implWithInst.containsKey(type))
+                        .findFirst()
+                        .get();
+            }
+            implementationClass = (Class<? extends I>) implementationWithInstanceMap.keySet()
+                    .iterator()
+                    .next();
+            if (implementationWithInstanceMap.get(implementationClass) != null) {
+                return (I) implementationWithInstanceMap.get(implementationClass);
+            }
+            if (implementationClass != null) {
+                I preparedInstance = beanFactory.createInstance(implementationClass);
+                Map<Class<?>, Object> implementationWithInstance = new HashMap<>();
+                implementationWithInstance.put(implementationClass, preparedInstance);
+                if (type.isInterface()) {
+                    interfaceAndImplementationWithInstanceMap.replace(type, implementationWithInstance);
+                } else {
+                    Class<?> keyOfClass = getKeyByOldValue(implementationWithInstanceMap);
+                    interfaceAndImplementationWithInstanceMap.replace(keyOfClass, implementationWithInstance);
+                }
+                return preparedInstance;
+            }
+            throw new RuntimeException("No implementation found");
+        }
+        throw new RuntimeException("No @PleaseService annotation present");
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
+
+    public Map<String, String> getPropertiesMap() {
+        return propertiesMap;
+    }
+
+    private Class<?> getKeyByOldValue(Map<Class<?>, Object> implementationWithInstanceMap) {
+        for (Map.Entry<Class<?>, Map<Class<?>, Object>> entry : interfaceAndImplementationWithInstanceMap.entrySet()) {
+            if (entry.getValue().equals(implementationWithInstanceMap)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+}
+
---
+++ custom_dependency_injection/src/main/java/com/epam/di/factory/BeanFactory.java
@@ -0,0 +1,55 @@
+package com.epam.di.factory;
+
+import com.epam.di.DependencyInjectionContext;
+import com.epam.di.configurator.AnnotationConfigurator;
+import org.reflections.Reflections;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class BeanFactory {
+
+    private final DependencyInjectionContext context;
+    private final List<AnnotationConfigurator> annotationConfigurators = new ArrayList<>();
+
+    public BeanFactory(DependencyInjectionContext context) {
+        this.context = context;
+        initAnnotationConfigurators();
+    }
+
+    public <I> I createInstance(Class<I> implementation) {
+        I instance = create(implementation);
+        configure(instance);
+        return instance;
+    }
+
+    private void initAnnotationConfigurators() {
+        Reflections reflections = new Reflections(context.getClass());
+        Set<Class<? extends AnnotationConfigurator>> subTypesOfAnnotationConfigurator = reflections.getSubTypesOf(AnnotationConfigurator.class);
+        for (Class<? extends AnnotationConfigurator> subType : subTypesOfAnnotationConfigurator) {
+            try {
+                annotationConfigurators.add(subType.getDeclaredConstructor().newInstance());
+            } catch (Exception e) {
+                throw new RuntimeException("An error occurred during object configuration " + e.getMessage());
+            }
+        }
+    }
+
+    private <I> I create(Class<I> implementation) {
+        try {
+            Constructor<I> constructor = implementation.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException("Cannot create object " + e.getMessage());
+        }
+    }
+
+    private <I> void configure(I implementation) {
+        annotationConfigurators.forEach(configurator -> configurator.configure(implementation, context));
+    }
+}
