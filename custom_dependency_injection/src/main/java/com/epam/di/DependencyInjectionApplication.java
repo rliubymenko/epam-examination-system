a3693ab5d7e0c4a75866fdf7f0d95b16e433a414
rliubymenko <rliubymenko@gmail.com>
2022-12-20 22:52:28 +0200

    gathered together all configurations for convenient use of dependency injection application; exposes method run that start point for application

---
+++ custom_dependency_injection/src/main/java/com/epam/di/DependencyInjectionApplication.java
@@ -0,0 +1,16 @@
+package com.epam.di;
+
+import com.epam.di.factory.BeanFactory;
+
+public class DependencyInjectionApplication {
+
+    private DependencyInjectionApplication() {
+    }
+
+    public static void run(Class<?> configClass) {
+        DependencyInjectionContext applicationContext = new DependencyInjectionContext(configClass);
+        BeanFactory beanFactory = new BeanFactory(applicationContext);
+        applicationContext.setBeanFactory(beanFactory);
+        applicationContext.applicationInitialization();
+    }
+}
