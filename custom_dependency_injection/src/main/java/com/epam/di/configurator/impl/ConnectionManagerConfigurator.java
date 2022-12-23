a9f02cad132e4ec4bdccf49b994b789437328d19
rliubymenko <rliubymenko@gmail.com>
2022-12-23 18:52:31 +0200

    changed annotation configurators for using AnnotationConfiguratorException class and added configurator for ConnectionPoolManager enum

--- custom_dependency_injection/src/main/java/com/epam/di/configurator/impl/ConnectionManagerConfigurator.java
+++ custom_dependency_injection/src/main/java/com/epam/di/configurator/impl/ConnectionManagerConfigurator.java
@@ -1,22 +1,23 @@
 package com.epam.di.configurator.impl;
 
-import com.epam.di.annotation.PleaseInject;
+import com.epam.di.annotation.PleaseConnectionManager;
 import com.epam.di.configurator.AnnotationConfigurator;
+import com.epam.di.connectionpool.ConnectionPoolManager;
 import com.epam.di.context.DependencyInjectionContext;
 import com.epam.di.exception.AnnotationConfiguratorException;
 
 import java.lang.reflect.Field;
 
-public class InjectAnnotationConfigurator implements AnnotationConfigurator {
+public class ConnectionManagerConfigurator implements AnnotationConfigurator {
 
     @Override
     public void configure(Object candidateForInject, DependencyInjectionContext context) {
         Field[] fields = candidateForInject.getClass().getDeclaredFields();
         for (Field field : fields) {
-            if (field.isAnnotationPresent(PleaseInject.class)) {
+            if (field.isAnnotationPresent(PleaseConnectionManager.class)) {
                 field.setAccessible(true);
                 try {
-                    field.set(candidateForInject, context.getInstance(field.getType()));
+                    field.set(candidateForInject, ConnectionPoolManager.INSTANCE);
                 } catch (IllegalAccessException e) {
                     throw new AnnotationConfiguratorException(field.getName(), candidateForInject.getClass().getSimpleName());
                 }
--- custom_dependency_injection/src/main/java/com/epam/di/configurator/impl/ValueAnnotationConfigurator.java
+++ custom_dependency_injection/src/main/java/com/epam/di/configurator/impl/ValueAnnotationConfigurator.java
@@ -1,12 +1,16 @@
 package com.epam.di.configurator.impl;
 
-import com.epam.di.context.DependencyInjectionContext;
 import com.epam.di.annotation.PleaseValue;
 import com.epam.di.configurator.AnnotationConfigurator;
+import com.epam.di.context.DependencyInjectionContext;
+import com.epam.di.environment.PropertyResolver;
+import com.epam.di.exception.AnnotationConfiguratorException;
 
 import java.lang.reflect.Field;
 
 public class ValueAnnotationConfigurator implements AnnotationConfigurator {
+
+    private static final PropertyResolver propertyResolver = PropertyResolver.getInstance();
 
     @Override
     public void configure(Object candidateForInject, DependencyInjectionContext context) {
@@ -15,13 +19,13 @@ public class ValueAnnotationConfigurator implements AnnotationConfigurator {
             if (declaredField.isAnnotationPresent(PleaseValue.class)) {
                 PleaseValue properties = declaredField.getAnnotation(PleaseValue.class);
                 String propertyName = properties.value();
-                String property = context.getPropertiesMap().get(propertyName);
+                String property = propertyResolver.getProperty(propertyName);
                 if (!property.isBlank()) {
                     declaredField.setAccessible(true);
                     try {
                         declaredField.set(candidateForInject, property);
                     } catch (IllegalAccessException e) {
-                        throw new RuntimeException("Exception while injecting property " + propertyName + " into " + candidateForInject.getClass().getSimpleName());
+                        throw new AnnotationConfiguratorException(propertyName, candidateForInject.getClass().getSimpleName());
                     }
                 }
             }
