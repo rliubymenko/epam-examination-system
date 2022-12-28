6f5807c3aa9e27f51406b1ec1890d767f52cd25c
rliubymenko <rliubymenko@gmail.com>
2022-12-28 23:51:35 +0200

    removed mark class

--- core_examination_system/src/main/java/com/epam/examinationsystem/core/dao/MarkDao.java
+++
@@ -1,7 +0,0 @@
-package com.epam.examinationsystem.core.dao;
-
-import com.epam.examinationsystem.core.dao.common.CommonDao;
-import com.epam.examinationsystem.core.entity.Mark;
-
-public interface MarkDao extends CommonDao<Mark> {
-}
