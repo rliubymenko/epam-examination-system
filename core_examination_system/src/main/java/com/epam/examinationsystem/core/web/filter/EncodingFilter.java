bf3bec764f38338aa63e21e5307095241ef01ba7
rliubymenko <rliubymenko@gmail.com>
2023-01-07 14:00:37 +0200

    added encoding WebInit param for filter

--- core_examination_system/src/main/java/com/epam/examinationsystem/core/web/filter/EncodingFilter.java
+++ core_examination_system/src/main/java/com/epam/examinationsystem/core/web/filter/EncodingFilter.java
@@ -2,22 +2,28 @@ package com.epam.examinationsystem.core.web.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebInitParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-@WebFilter("/*")
+@WebFilter(urlPatterns = "/*", initParams = @WebInitParam(name = "encoding", value = "UTF-8"))
 public class EncodingFilter implements Filter {
 
     private static final Logger LOG = LoggerFactory.getLogger(EncodingFilter.class);
-    private static final String ENCODING = "UTF-8";
+    private String encoding;
+
+    @Override
+    public void init(FilterConfig config) {
+        encoding = config.getInitParameter("encoding");
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         LOG.debug("Encoding request and response");
-        request.setCharacterEncoding(ENCODING);
-        response.setCharacterEncoding(ENCODING);
+        request.setCharacterEncoding(encoding);
+        response.setCharacterEncoding(encoding);
         chain.doFilter(request, response);
     }
 }
