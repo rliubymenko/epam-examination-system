0b30dc2b91ae70350385caadc9a2b93144145744
rliubymenko <rliubymenko@gmail.com>
2023-02-27 15:12:48 +0200

    added filtering of the tests for student

--- core_examination_system/src/main/java/com/epam/examinationsystem/core/dao/TestDao.java
+++ core_examination_system/src/main/java/com/epam/examinationsystem/core/dao/TestDao.java
@@ -1,6 +1,7 @@
 package com.epam.examinationsystem.core.dao;
 
 import com.epam.examinationsystem.core.dao.common.CommonDao;
+import com.epam.examinationsystem.core.datatable.DataTableRequest;
 import com.epam.examinationsystem.core.entity.Test;
 import com.epam.examinationsystem.core.exception.DaoException;
 
@@ -12,7 +13,13 @@ public interface TestDao extends CommonDao<Test> {
 
     List<Test> findAllBySubjectUuid(UUID uuid) throws DaoException;
 
+    List<Test> findAllForStudent() throws DaoException;
+
+    List<Test> findAllForStudent(DataTableRequest request) throws DaoException;
+
     Optional<Test> findByUuidWithoutSubject(UUID uuid) throws DaoException;
+
+    long countForStudent(DataTableRequest request) throws DaoException;
 
     boolean incrementTotalAttemptNumber(Test test) throws DaoException;
 }
--- core_examination_system/src/main/java/com/epam/examinationsystem/core/dao/impl/TestDaoImpl.java
+++ core_examination_system/src/main/java/com/epam/examinationsystem/core/dao/impl/TestDaoImpl.java
@@ -5,6 +5,7 @@ import com.epam.di.annotation.PleaseService;
 import com.epam.examinationsystem.core.dao.SubjectDao;
 import com.epam.examinationsystem.core.dao.TestDao;
 import com.epam.examinationsystem.core.dao.common.AbstractDao;
+import com.epam.examinationsystem.core.datatable.DataTableRequest;
 import com.epam.examinationsystem.core.entity.Subject;
 import com.epam.examinationsystem.core.entity.Test;
 import com.epam.examinationsystem.core.enumeration.DaoConstant;
@@ -12,6 +13,7 @@ import com.epam.examinationsystem.core.exception.DaoException;
 import com.epam.examinationsystem.core.util.LoggerUtil;
 import com.epam.examinationsystem.core.util.db.DaoMapperUtil;
 import com.epam.examinationsystem.core.util.db.QueryBuilderUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,9 +28,37 @@ import java.util.*;
 public class TestDaoImpl extends AbstractDao<Test> implements TestDao {
 
     private static final String ENTITY_NAME = "test";
+    private static final String WHERE_CLAUSE = "where";
     private static final String SUBJECT_ID = "subject_id";
     private static final Map<String, String> foreignTableNamesWithKeys = Map.of("subject", SUBJECT_ID);
     private static final Logger LOG = LoggerFactory.getLogger(TestDaoImpl.class);
+    private static final String FIND_ALL_READY_TO_PASS_TESTS_BY_SUBJECT_UUID = """
+            SELECT *
+            FROM test
+                     LEFT JOIN subject ON test.subject_id = subject.id
+            WHERE test.id NOT IN (SELECT DISTINCT test.id
+                                  FROM test
+                                           LEFT JOIN question ON question.test_id = test.id
+                                           LEFT JOIN answer ON answer.question_id = question.id
+                                  where answer.id IS NULL OR question.id IS NULL)
+                AND subject.id = """;
+    private static final String FIND_ALL_READY_TO_PASS_TESTS = """
+            SELECT *
+            FROM test
+            WHERE test.id NOT IN (SELECT DISTINCT test.id
+                                  FROM test
+                                           LEFT JOIN question ON question.test_id = test.id
+                                           LEFT JOIN answer ON answer.question_id = question.id
+                                  where answer.id IS NULL OR question.id IS NULL);
+                                  """;
+    private static final String FIND_ALL_READY_TO_PASS_QUERY_PART = """
+            test.id NOT IN (SELECT DISTINCT test.id
+                                  FROM test
+                                           LEFT JOIN question ON question.test_id = test.id
+                                           LEFT JOIN answer ON answer.question_id = question.id
+                                  where answer.id IS NULL OR question.id IS NULL)
+            """;
+
 
     @PleaseInject
     private SubjectDao subjectDao;
@@ -57,6 +87,7 @@ public class TestDaoImpl extends AbstractDao<Test> implements TestDao {
         LoggerUtil.findByIdStartLogging(LOG, ENTITY_NAME, id);
         try (Statement statement = connection.createStatement()) {
             String findQuery = QueryBuilderUtil.findByIdQuery(DaoConstant.TEST_TABLE_NAME.getValue(), id);
+
             try (ResultSet resultSet = statement.executeQuery(findQuery)) {
                 test = extractTestWithoutSubject(resultSet);
             }
@@ -65,6 +96,56 @@ public class TestDaoImpl extends AbstractDao<Test> implements TestDao {
             throw new DaoException(message, e);
         }
         return test;
+    }
+
+    @Override
+    public List<Test> findAllForStudent(DataTableRequest request) throws DaoException {
+        List<Test> entities;
+        LoggerUtil.findAllByParametersStartLogging(LOG, ENTITY_NAME, request);
+        try (Statement statement = connection.createStatement()) {
+            String findAllQuery = QueryBuilderUtil.findAllAndJoinTableByForeignKeyByQueryParameters(DaoConstant.TEST_TABLE_NAME.getValue(), request, foreignTableNamesWithKeys);
+            findAllQuery = addReadyToTestFilter(findAllQuery);
+            try (ResultSet resultSet = statement.executeQuery(findAllQuery)) {
+                entities = extractEntities(resultSet);
+            }
+        } catch (SQLException e) {
+            String message = LoggerUtil.findAllByParametersErrorLogging(LOG, ENTITY_NAME, request);
+            throw new DaoException(message, e);
+        }
+        return entities;
+    }
+
+    @Override
+    public List<Test> findAllForStudent() throws DaoException {
+        List<Test> entities;
+        LoggerUtil.findAllStartLogging(LOG, ENTITY_NAME);
+        try (Statement statement = connection.createStatement()) {
+            try (ResultSet resultSet = statement.executeQuery(FIND_ALL_READY_TO_PASS_TESTS)) {
+                entities = extractEntities(resultSet);
+            }
+        } catch (SQLException e) {
+            String message = LoggerUtil.findAllErrorLogging(LOG, ENTITY_NAME);
+            throw new DaoException(message, e);
+        }
+        return entities;
+    }
+
+    @Override
+    public long countForStudent(DataTableRequest request) throws DaoException {
+        LoggerUtil.countingRecordsStartLogging(LOG, ENTITY_NAME);
+        try (Statement statement = connection.createStatement()) {
+            String countQuery = QueryBuilderUtil.countQuery(DaoConstant.TEST_TABLE_NAME.getValue(), request, foreignTableNamesWithKeys);
+            countQuery = addReadyToTestFilter(countQuery);
+            try (ResultSet resultSet = statement.executeQuery(countQuery)) {
+                if (resultSet.next()) {
+                    return resultSet.getLong("count");
+                }
+            }
+        } catch (SQLException e) {
+            String message = LoggerUtil.countingRecordsErrorLogging(LOG, ENTITY_NAME);
+            throw new DaoException(message, e);
+        }
+        return 0;
     }
 
     @Override
@@ -91,7 +172,7 @@ public class TestDaoImpl extends AbstractDao<Test> implements TestDao {
         String subjectId = String.valueOf(subject.getId());
         LoggerUtil.findByStartLogging(LOG, ENTITY_NAME, SUBJECT_ID, subjectId);
         try (Statement statement = connection.createStatement()) {
-            String findAllQuery = QueryBuilderUtil.findByQuery(DaoConstant.TEST_TABLE_NAME.getValue(), SUBJECT_ID, subjectId);
+            String findAllQuery = FIND_ALL_READY_TO_PASS_TESTS_BY_SUBJECT_UUID + subjectId + ";";
             try (ResultSet resultSet = statement.executeQuery(findAllQuery)) {
                 tests = extractTestsWithoutSubject(resultSet);
             }
@@ -185,5 +266,22 @@ public class TestDaoImpl extends AbstractDao<Test> implements TestDao {
             throw new DaoException(message);
         }
         return maybeSubject.get();
+    }
+
+    private String addReadyToTestFilter(String query) {
+        if (StringUtils.containsIgnoreCase(query, WHERE_CLAUSE)) {
+            int whereIndex = StringUtils.indexOfIgnoreCase(query, WHERE_CLAUSE) + WHERE_CLAUSE.length();
+            return query.substring(0, whereIndex) +
+                    " " +
+                    FIND_ALL_READY_TO_PASS_QUERY_PART +
+                    " AND " +
+                    query.substring(whereIndex);
+        }
+        int whereIndex = StringUtils.indexOfIgnoreCase(query, "subject.id") + "subject.id".length();
+        return query.substring(0, whereIndex) +
+                " WHERE " +
+                FIND_ALL_READY_TO_PASS_QUERY_PART +
+                " " +
+                query.substring(whereIndex);
     }
 }
